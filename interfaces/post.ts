17a0097ff2300154cda0f2b1492710869d879031
Astra <abhaypratapsinghwd@gmail.com>
2025-08-09 18:55:44 +0530

    dep bug fix

--- interfaces/post.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from "@prisma/client";
-
-export interface PostWithAuthorAndSkeleton extends Post {
-  author: {
-    id: string;
-    name: string;
-  };
-  isSkeleton?: boolean;
-}
